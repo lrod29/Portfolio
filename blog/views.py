4ef24f5d0a731171218da8249ed0342e640b9c71
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-23 14:46:10 -0400

    primera vista y template del blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
 def render_posts(request):
-    return render(request, 'posts.html')
+    posts = Post.objects.all()
+    return render(request, 'posts.html', {'posts': posts})
