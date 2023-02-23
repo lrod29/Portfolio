09c793210a162824909da4a9fa46caf7aa3a72c5
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-23 19:31:50 -0400

    pasos por parametro para los post del blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 
-from .views import render_posts
+from .views import render_posts, post_detail
 
 urlpatterns = [
-    path('', render_posts, name='posts')
+    path('', render_posts, name='posts'),
+    path('post/<int:post_id>', post_detail, name='post_detail')
 ]
