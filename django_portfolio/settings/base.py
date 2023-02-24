9f363054d1d99cc99094de9d9418b00a9a6b29e6
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-24 00:48:12 -0400

    agregar variable de entorno

--- django_portfolio/settings/base.py
+++ django_portfolio/settings/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -22,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gvddqy+f3$)u1=n#!ry!s8qn8*xc1wp)n9t0#3mkw&oaw%a*wp'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
